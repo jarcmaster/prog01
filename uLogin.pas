98cd3ae0653c1dc819db76c01fc1d83c4417d6b5
jarcmaster <jarcmaster@gmail.com>
2016-07-20 01:06:06 -0400

    Botones Login y Close
    Se agregaron los botones y el codigo para Teminar la APP

--- uLogin.pas
+++ uLogin.pas
@@ -12,6 +12,10 @@ type
     Edit2: TEdit;
     Label1: TLabel;
     Label2: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,5 +28,19 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TfrmLogin.Button1Click(Sender: TObject);
+begin
+   //Codiog de evaluar el usuario aqui
+   {
+   ...
+   }
+end;
+
+procedure TfrmLogin.Button2Click(Sender: TObject);
+begin
+   //Finalizar el programa
+   Application.Terminate ;
+end;
 
 end.
